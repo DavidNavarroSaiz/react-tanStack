8eea779782a9bc31243ead339bc407484edc0805
David-Sol-AI <davidcnavarrosaiz@elsolnec.org>
2024-02-15 18:23:33 -0500

    pagination working

--- src/components/TaskTable.jsx
+++ src/components/TaskTable.jsx
@@ -19,47 +19,103 @@ const columns = [
   {
     accessorKey: 'task',
     header: "Task",
-    cell: (props) => <p>{props.getValue()}</p>
+    size: 225,
+    cell: EditableCell,
+    enableColumnFilter: true,
+    filterFn: "includesString"
   },
   {
     accessorKey: 'status',
     header: "Status",
-    cell: (props) => <p>{props.getValue()?.name}</p>
+    cell: StatusCell,
+    enableSorting: false,
+    enableColumnFilter: true,
+    filterFn: (row, columnId, filterStatuses) => {
+      if (filterStatuses.lenght === 0) return true
+      const status = row.getValue(columnId);
+      return filterStatuses.includes(status?.id)
+    }
   },
   {
     accessorKey: 'due',
     header: "Due",
-    cell: (props) => <p>{props.getValue()?.toLocaleTimeString()}</p>
+    cell: DateCell
   },
   {
     accessorKey: 'notes',
     header: "Notes",
-    cell: (props) => <p>{props.getValue()}</p>
+    size: 225,
+    cell: EditableCell
   },
 
 ]
 
 const TaskTable = () => {
   const [data, setData] = useState(DATA)
+  const [columnFilters, setColumnFilters] = useState([])
   const table = useReactTable({
     data,
     columns,
-    getCoreRowModel: getCoreRowModel()
+    state: {
+      columnFilters,
+      pagination:{
+        pageSize:10,
+        pageIndex: 0
+      }
+    },
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    columnResizeMode: "onChange",
+    meta: {
+      updateData: (rowIndex, columnId, value) => setData(
+        prev => prev.map((row, index) =>
+          index === rowIndex ? {
+            ...prev[rowIndex],
+            [columnId]: value,
+          } : row
+        )
+      )
+    }
   })
-  // console.log(table.getRowModel().rows[0].getVisibleCells()[0])
+  console.log(data)
   return (
     <Box>
+      <Filters columnFilters={columnFilters} setColumnFilters={setColumnFilters} />
+
       <Box className="table" w={table.getTotalSize()}>
         {table.getHeaderGroups().map(headerGroup => (
           <Box className="tr" key={headerGroup.id}>
             {headerGroup.headers.map((header) => (
-              <Box className="th"  w={header.getSize()}  key={header.id}>
+              <Box className="th" w={header.getSize()} key={header.id}>
                 {header.column.columnDef.header}
+                {
+                  header.column.getCanSort() && <Icon as={SortIcon} mx={3} fontSize={14}
+                    onClick={header.column.getToggleSortingHandler()}
+                  />
+                }
+                {
+                  {
+                    asc: " 🔼",
+                    desc: " 🔽",
+                  }[header.column.getIsSorted()]
+                  
+                }
+                <Box
+                  onMouseDown={
+                    header.getResizeHandler()
+                  }
+                  onTouchStart={header.getResizeHandler()}
+                  className={`resizer ${header.column.getIsResizing() ? "isResizing" : ""
+                    }`}>
+
+                </Box>
               </Box>
             ))}
           </Box>))}
         {table.getRowModel().rows.map(row_table => (<Box className="tr" key={row_table.id}>
-          {row_table.getVisibleCells().map(cell => (<Box className="td" w= {cell.column.getSize()} key={cell.id}>
+          {row_table.getVisibleCells().map(cell => (<Box className="td" w={cell.column.getSize()} key={cell.id}>
             {flexRender(cell.column.columnDef.cell, cell.getContext())}
           </Box>
           ))}
@@ -67,6 +123,21 @@ const TaskTable = () => {
         ))}
 
       </Box>
+      <Text mb={2}>
+        <br />
+        Page 
+       {table.getState().pagination.pageIndex + 1}of {" "}        
+       {table.getPageCount()}      
+      </Text>
+
+      <ButtonGroup size="sm" isAttached variant="outline">
+        <Button onClick={() =>table.previousPage()} isDisabled={!table.getCanPreviousPage()}>
+          {"<"}
+        </Button>
+        <Button onClick={() =>table.nextPage()} isDisabled={!table.getCanNextPage()}>
+          {">"}
+        </Button>
+      </ButtonGroup>
     </Box>
   );
 };
