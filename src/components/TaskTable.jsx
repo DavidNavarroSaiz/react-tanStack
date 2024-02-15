1fe9ba8ec6f986c90396df2e65173db835b04cfe
David-Sol-AI <davidcnavarrosaiz@elsolnec.org>
2024-02-15 12:17:33 -0500

    created basic table

--- src/components/TaskTable.jsx
+++ src/components/TaskTable.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Box, Button, ButtonGroup, Icon, Text } from "@chakra-ui/react";
 import {
   flexRender,
@@ -14,137 +13,60 @@ import StatusCell from "./StatusCell";
 import DateCell from "./DateCell";
 import Filters from "./Filters";
 import SortIcon from "./icons/SortIcon";
+import { useState } from "react";
 
 const columns = [
   {
-    accessorKey: "task",
+    accessorKey: 'task',
     header: "Task",
-    size: 225,
-    cell: EditableCell,
-    enableColumnFilter: true,
-    filterFn: "includesString",
+    cell: (props) => <p>{props.getValue()}</p>
   },
   {
-    accessorKey: "status",
+    accessorKey: 'status',
     header: "Status",
-    cell: StatusCell,
-    enableSorting: false,
-    enableColumnFilter: true,
-    filterFn: (row, columnId, filterStatuses) => {
-      if (filterStatuses.length === 0) return true;
-      const status = row.getValue(columnId);
-      return filterStatuses.includes(status?.id);
-    },
+    cell: (props) => <p>{props.getValue()?.name}</p>
   },
   {
-    accessorKey: "due",
+    accessorKey: 'due',
     header: "Due",
-    cell: DateCell,
+    cell: (props) => <p>{props.getValue()?.toLocaleTimeString()}</p>
   },
   {
-    accessorKey: "notes",
+    accessorKey: 'notes',
     header: "Notes",
-    size: 225,
-    cell: EditableCell,
+    cell: (props) => <p>{props.getValue()}</p>
   },
-];
+
+]
 
 const TaskTable = () => {
-  const [data, setData] = useState(DATA);
-  const [columnFilters, setColumnFilters] = useState([]);
-
+  const [data, setData] = useState(DATA)
   const table = useReactTable({
     data,
     columns,
-    state: {
-      columnFilters,
-    },
-    getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    columnResizeMode: "onChange",
-    meta: {
-      updateData: (rowIndex, columnId, value) =>
-        setData((prev) =>
-          prev.map((row, index) =>
-            index === rowIndex
-              ? {
-                  ...prev[rowIndex],
-                  [columnId]: value,
-                }
-              : row
-          )
-        ),
-    },
-  });
-
+    getCoreRowModel: getCoreRowModel()
+  })
+  // console.log(table.getRowModel().rows[0].getVisibleCells()[0])
   return (
     <Box>
-      <Filters
-        columnFilters={columnFilters}
-        setColumnFilters={setColumnFilters}
-      />
       <Box className="table" w={table.getTotalSize()}>
-        {table.getHeaderGroups().map((headerGroup) => (
+        {table.getHeaderGroups().map(headerGroup => (
           <Box className="tr" key={headerGroup.id}>
             {headerGroup.headers.map((header) => (
-              <Box className="th" w={header.getSize()} key={header.id}>
+              <Box className="th"  w={header.getSize()}  key={header.id}>
                 {header.column.columnDef.header}
-                {header.column.getCanSort() && (
-                  <Icon
-                    as={SortIcon}
-                    mx={3}
-                    fontSize={14}
-                    onClick={header.column.getToggleSortingHandler()}
-                  />
-                )}
-                {
-                  {
-                    asc: " 🔼",
-                    desc: " 🔽",
-                  }[header.column.getIsSorted()]
-                }
-                <Box
-                  onMouseDown={header.getResizeHandler()}
-                  onTouchStart={header.getResizeHandler()}
-                  className={`resizer ${
-                    header.column.getIsResizing() ? "isResizing" : ""
-                  }`}
-                />
               </Box>
             ))}
+          </Box>))}
+        {table.getRowModel().rows.map(row_table => (<Box className="tr" key={row_table.id}>
+          {row_table.getVisibleCells().map(cell => (<Box className="td" w= {cell.column.getSize()} key={cell.id}>
+            {flexRender(cell.column.columnDef.cell, cell.getContext())}
           </Box>
+          ))}
+        </Box>
         ))}
-        {table.getRowModel().rows.map((row) => (
-          <Box className="tr" key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <Box className="td" w={cell.column.getSize()} key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </Box>
-            ))}
-          </Box>
-        ))}
+
       </Box>
-      <br />
-      <Text mb={2}>
-        Page {table.getState().pagination.pageIndex + 1} of{" "}
-        {table.getPageCount()}
-      </Text>
-      <ButtonGroup size="sm" isAttached variant="outline">
-        <Button
-          onClick={() => table.previousPage()}
-          isDisabled={!table.getCanPreviousPage()}
-        >
-          {"<"}
-        </Button>
-        <Button
-          onClick={() => table.nextPage()}
-          isDisabled={!table.getCanNextPage()}
-        >
-          {">"}
-        </Button>
-      </ButtonGroup>
     </Box>
   );
 };
